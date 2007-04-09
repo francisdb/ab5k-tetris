85f5cf3517de78d611309590c48eccdbca333951
francisdb <francisdb@1518ea5e-382d-0410-aa7e-057b88573aa0>
2007-04-09 21:15:55 +0000

    Changes for the new desklet spec trunk version

--- trunk/src/main/java/eu/somatik/desklet/tetris/TetrisDesklet.java
+++ trunk/src/main/java/eu/somatik/desklet/tetris/TetrisDesklet.java
@@ -9,15 +9,15 @@
 
 package eu.somatik.desklet.tetris;
 
-import ab5k.desklet.AbstractDesklet;
-import ab5k.desklet.DeskletContext;
+import ab5k.desklet.Desklet;
+import ab5k.desklet.DeskletContainer;
 import ab5k.desklet.test.DeskletTester;
 
 /**
  *
  * @author francisdb
  */
-public class TetrisDesklet extends AbstractDesklet{
+public class TetrisDesklet extends Desklet{
     
     private TetrisPanel tetrisPanel;
     
@@ -28,15 +28,19 @@ public class TetrisDesklet extends AbstractDesklet{
     public static void main(String s[]) {
         DeskletTester.start(TetrisDesklet.class);
     }
-
-    public void init(DeskletContext context) throws Exception {
-        this.context = context;
+    
+    /**
+     * Sets up the desklet
+     * @throws java.lang.Exception 
+     */
+    public void init() throws Exception {
         tetrisPanel = new TetrisPanel();
-        context.getContainer().setBackgroundDraggable(true);
-        context.getContainer().setContent(tetrisPanel);
-        context.getContainer().setResizable(false);
-        context.getContainer().setShaped(true);    
-        context.getContainer().setVisible(true);
+        DeskletContainer container = getContext().getContainer();
+        getContext().getContainer().setBackgroundDraggable(true);
+        container.setContent(tetrisPanel);
+        container.setResizable(false);
+        container.setShaped(true);    
+        container.setVisible(true);
     }
 
     public void start() throws Exception {
@@ -45,7 +49,7 @@ public class TetrisDesklet extends AbstractDesklet{
 
     public void stop() throws Exception {
         tetrisPanel.stop();
-        this.context.notifyStopped();
+        getContext().notifyStopped();
     }
 
     public void destroy() throws Exception {
