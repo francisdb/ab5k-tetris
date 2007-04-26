b2f163d824878ecc0498f55cde03d3a5b63ee63d
francisdb <francisdb@1518ea5e-382d-0410-aa7e-057b88573aa0>
2007-04-26 22:32:12 +0000

    run desklet on swing EDT fix

--- src/main/java/eu/somatik/desklet/tetris/TetrisDesklet.java
+++ src/main/java/eu/somatik/desklet/tetris/TetrisDesklet.java
@@ -12,6 +12,8 @@ package eu.somatik.desklet.tetris;
 import ab5k.desklet.Desklet;
 import ab5k.desklet.DeskletContainer;
 import ab5k.desklet.test.DeskletTester;
+import java.lang.reflect.InvocationTargetException;
+import javax.swing.SwingUtilities;
 
 /**
  *
@@ -34,13 +36,31 @@ public class TetrisDesklet extends Desklet{
      * @throws java.lang.Exception 
      */
     public void init() throws Exception {
-        tetrisPanel = new TetrisPanel();
-        DeskletContainer container = getContext().getContainer();
-        getContext().getContainer().setBackgroundDraggable(true);
-        container.setContent(tetrisPanel);
-        container.setResizable(false);
-        container.setShaped(true);    
-        container.setVisible(true);
+        runOnEDT( new Runnable(){
+            public void run() {
+                tetrisPanel = new TetrisPanel();
+                DeskletContainer container = getContext().getContainer();
+                getContext().getContainer().setBackgroundDraggable(true);
+                container.setContent(tetrisPanel);
+                container.setResizable(false);
+                container.setShaped(true);    
+                container.setVisible(true);
+            }
+        });
+    }
+    
+    private void runOnEDT(Runnable runnable){
+        if(SwingUtilities.isEventDispatchThread()){
+            runnable.run();
+        }else{
+            try         {
+                javax.swing.SwingUtilities.invokeAndWait(runnable);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            } catch (InvocationTargetException ex) {
+                ex.printStackTrace();
+            }
+}
     }
 
     public void start() throws Exception {
